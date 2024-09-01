b59aba898b791febaee1fd47764c8a5eed48810f
Md. Hafiz Al Asad <55018310+mdhafizalasad@users.noreply.github.com>
2024-09-01 16:56:44 +0600

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,18 @@
+document.getElementById('submit-buttton').addEventListener('click', function (){
+    // user email get
+    const userInput = document.getElementById("user-email");
+    const userEmail = userInput.value;
+    // console.log(userEmail);
+
+// user password get
+const passwordInputField = document.getElementById('user-password');
+const userPassword = passwordInputField.value;
+//console.log(userPassword);
+
+if (userEmail == 'admin@gmail.com' && userPassword == '123456'){
+    alert("Login Successful");
+    window.location.href = "../banking.html"
+}else if (userEmail !='admin@gmail.com' || userPassword != '123456'){
+    alert("Username or password is not valid");
+}
+})
